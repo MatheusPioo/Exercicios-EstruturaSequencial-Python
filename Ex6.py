ac9bed0fb22ad15daa993dd37aaac8376925f208
Matheus Pio <matheus.ap.pio@gmail.com>
2024-09-23 12:32:32 -0300

    Create Ex6.py

---
+++ Ex6.py
@@ -0,0 +1,5 @@
+# 6. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+print('Calculo da area de um circulo')
+raio = float(input('Entre com o raio do circulo: '))
+area = 3.14 * (raio ** 2)
+print(f'A area do circulo informado é: {area}')
