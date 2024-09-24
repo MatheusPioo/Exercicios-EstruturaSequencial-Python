aebefae5e41352496d287dda68294d6bf895b169
Matheus Pio <matheus.ap.pio@gmail.com>
2024-09-24 12:40:14 -0300

    Create Ex7.py

---
+++ Ex7.py
@@ -0,0 +1,6 @@
+'''Faça um Programa que calcule a area de um quadrado, em seguida mostre o dobro desta area para o usuario.'''
+print("Calculo da area do quadrado e o dobro da area \n")
+base = float(input("Entre com o valor de um lado do quadrado "))
+base **= 2
+dobro = base * 2
+print(f"O valor da area do quadrado é {base:.2f} e o dobro desse valor é {dobro:.2f} ")
